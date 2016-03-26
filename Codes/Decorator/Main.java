ff4077f3066da6faea23fd20f8e141a6f875aea6
Adrian R. Bazaga <alu0100826456@ull.edu.es>
2016-03-26 12:59:04 +0000

    Added commentaries to Decorator Main class

--- Codes/Decorator/Main.java
+++ Codes/Decorator/Main.java
@@ -7,19 +7,19 @@ package es.ull.esit.pai.exposition.Decorator;
 
 public class Main {
   public static void main(String[] args) {
-    Shape circle = new Circle();
+    Shape circle = new Circle();                                  // Creating a normal Circle
   
-    Shape redCircle = new RedShapeDecorator(new Circle());
+    Shape redCircle = new RedShapeDecorator(new Circle());        // Creating a Circle with red border
   
-    Shape redRectangle = new RedShapeDecorator(new Rectangle());
+    Shape redRectangle = new RedShapeDecorator(new Rectangle());  // Creating a Rectangle with red border
     
     System.out.println("Circle with normal border");
-    circle.draw();
+    circle.draw();          // Drawing the normal circle
     
     System.out.println("\nCircle of red border");
-    redCircle.draw();
+    redCircle.draw();       // Drawing the decorated Circle (with red border)
     
     System.out.println("\nRectangle of red border");
-    redRectangle.draw();  
+    redRectangle.draw();    // Drawing the decorated Rectangle (with red border)
   }
 }
